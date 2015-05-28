cba586ac85286e92f604e1d00e9b0a4c67fb98bf
Thakkar, Aakash <Aakash_Thakkar@intuit.com>
2015-05-28 15:24:06 -0500

    Adding config.js file for configuring MongoDB urls

---
+++ routes/config.js
@@ -0,0 +1,8 @@
+var config = {
+	mongo: {
+		local_url: 'mongodb://localhost:27017/starter-node-backend',
+		server_url: 'mongodb://serverurl:serverport/starter-node-backend'
+	}
+};
+
+module.exports = config;

ad61892a9ace4e68b9a63ba9c788d01ecfdfe0a4
Thakkar, Aakash <Aakash_Thakkar@intuit.com>
2015-05-28 16:43:26 -0500

    Adding URLs to the server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var resources = require('./routes/resources.js')
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -9,6 +10,13 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Context-Type, Accept");
 	next();
 });
+
+app.get('/health', resources.getHealthCheck);
+app.get('/resources', resources.getAll);
+app.get('/resources/:id', resources.getOneById)
+app.post('/resources', resources.createOne);
+app.delete('/resources/:id', resources.deleteOneById);
+app.update('/resources/:id', resources.updateOneById);
 
 var server = app.listen(3000, function() {
 	var host = server.address().address;

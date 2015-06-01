5d2a4ea23b75097bf8d2240af0d28d1333a71a25
Thakkar, Aakash <Aakash_Thakkar@intuit.com>
2015-06-01 10:57:45 -0500

    Routes for root using mount path

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var resources = require('./routes/resources.js')
 var bodyParser = require('body-parser');
 
+var rootapp = express();
 var app = express();
 
-app.use(bodyParser.json());
-app.use(function(req, res, next) {
+rootapp.use(bodyParser.json());
+rootapp.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Context-Type, Accept");
 	next();
@@ -18,7 +19,9 @@ app.post('/resources', resources.createOne);
 app.delete('/resources/:id', resources.deleteOneById);
 app.put('/resources/:id', resources.updateOneById);
 
-var server = app.listen(3000, function() {
+rootapp.use('/starter-node-backend', app);
+
+var server = rootapp.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Starter-node-backend app listening at http://%s:%s', host, port);
